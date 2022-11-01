57fdf776967003803129b46d09a49eac52d8308e
Azat Akhmetov <51468504+metov@users.noreply.github.com>
2022-10-31 20:01:55 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name="dotstree",
-    version="0.1.2",
+    version="0.1.3",
     description="Flexible dotfile manager based on directory trees.",
     long_description=Path("README.md").read_text(),
     long_description_content_type="text/markdown",
